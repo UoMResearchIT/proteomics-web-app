d3312a5f27e21e11c78234c1a6d8d0de2e33d0f4
emma <emmasimp@users.noreply.github.com>
2023-08-08 16:28:28 +0100

    Added tabs, theme and interactive plot function

--- protometrics_app/app.R
+++ protometrics_app/app.R
@@ -4,26 +4,53 @@ library(readxl)
 library(tidyverse)
 
 ui <- fluidPage(
-
-  fileInput("upload", NULL),
-  tableOutput("head"),
-  uiOutput("toCol"),
-  plotOutput("plot", click = "plot_click"),
-  verbatimTextOutput("info")
-
+  theme = bslib::bs_theme(bootswatch = "darkly"),
+  div(style = "height:50px"),
+  titlePanel('Lennon Lab Proteomic data archive'),
+  div(style = "height:50px"),
+  sidebarLayout(
+    sidebarPanel( 
+      fileInput("upload", label = 'Dataset'),
+    #  tableOutput("head"),
+      uiOutput("toCol"),
+      ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel('BoxPlot', 
+       plotOutput("plot", click = "plot_click"),
+      # verbatimTextOutput("info"),
+       tableOutput("near_rows_data")
+    ),
+      tabPanel('PCA', 'Plot ot be added'),
+      tabPanel('HeatMap', 'Plot to be added'),
+      tabPanel('Correlation','Plot ot be added')
+    )
+  ),
+  )
 )
 
 server <- function(input, output, session) {
+  thematic::thematic_shiny()
   
   data <- reactive({
     req(input$upload)
-    read_excel(input$upload$datapath)
+    read_excel(input$upload$datapath)[,-1] |>
+      pivot_longer(cols = !(UniprotID:gene.names),
+                   names_to = "experiment", 
+                   values_to = "expression")
   })
 
   output$head <- renderTable({
     head(data(), 5)
   })
-  
+  output$near_rows_data <- renderTable({
+    req(input$plot_click)
+    df <- data()
+    nearPoints(df[df$gene.names == input$gene_dropdown,],
+               input$plot_click,
+               maxpoints = 5)
+  })
   output$toCol <- renderUI({
     df <- data()
     items <- unique(df$gene.names)
@@ -33,16 +60,18 @@ server <- function(input, output, session) {
   output$plot <- renderPlot({
     req(input$gene_dropdown)
     df <- data()
-    df[,-1] |>
-      pivot_longer(cols = !(UniprotID:gene.names), 
-        names_to = "experiment", 
-        values_to = "expression") |>
+    df |>
       filter(gene.names == input$gene_dropdown) |>
       ggplot(aes(x = experiment, y = expression)) +
       geom_point()
   }, res = 96)
   
- 
+  output$info <- renderPrint({
+    req(input$plot_click)
+    x <- round(input$plot_click$x, 2)
+    y <- round(input$plot_click$y, 2)
+    cat("[", x, ", ", y, "]", sep = "")
+  })
 }
 
 shinyApp(ui, server)
