951bbec81becb262ce502dbb5d0a10a348a4ed19
fherreazcue <francisco.herreazcue@gmail.com>
2024-02-12 10:56:08 +0000

    Reordered plot_functions to reflect new tab order

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -4,6 +4,72 @@ font_family <- 'Courier'
 ##### Plotting scripts ####
 
 #### TAB 1 ####
+pca_plot <- function(df){
+  # create the group design
+  design <- data.frame("Samples" = seq(1:nrow(df)),
+                       "Group" = seq(1:nrow(df)))
+  design$Samples <- rownames(df)
+  design$Group <- gsub("_\\d+", "", design$Samples)
+
+  # Perform a PCA with pre-processed data
+  pca <- pca(X = df,
+             ncomp = nrow(df),
+             center = TRUE,
+             scale = TRUE)
+  # Create a 2D Sample Plot
+  p <- plotIndiv(object = pca,
+            comp = c(1,2),
+            ind.names = FALSE,
+            group = design$Group,
+            title = "Template data set",
+            legend = TRUE,
+            size.xlabel = rel(1.3),
+            size.ylabel = rel(1.3),
+            size.legend = rel(1.3),
+            cex = 0.8,
+            ellipse = TRUE)
+  return(p$graph)
+}
+
+#### TAB 2 ####
+function.heatmap <- function(matrix){
+  x <- as.matrix(matrix)
+  x.trunc <- as.vector(unique(x))
+  x.trim <- x.trunc[x.trunc >= quantile(x.trunc, probs = 0.01, na.rm = T) &
+                      x.trunc <= quantile(x.trunc, probs = 0.99, na.rm = T)]
+  x.max <- which.max(x.trim)
+  x.min <- which.min(x.trim)
+  # create a color function for the heatmap
+  col_fun <- circlize::colorRamp2(c(x.trim[x.min], 0, x.trim[x.max]),
+                                  c("blue", "white", "red"),
+                                  space = "sRGB")
+  # create labels for the heatmap
+  rowlab <- gsub(".*,\\s*", "", rownames(x)) # UniProt ID for row labels
+  col_lab <- gsub("_", " ", colnames(x)) # for sample annotation
+  col_group <- gsub("_\\d+", "", colnames(x)) |> # for group annotation
+    unlist() |>
+    as.factor()
+  # create heatmap
+  set.seed(3)
+  ht <- ComplexHeatmap::Heatmap(x,
+                                name = "Protein level",
+                                col = col_fun,
+                                show_row_names = T,
+                                row_labels = rowlab,
+                                show_column_names = F,
+                                row_title = "Proteins",
+                                row_title_gp = grid::gpar(fontsize = 12,
+                                                          fontface = "bold"),
+                                column_title = "",
+                                row_dend_width = unit(2, "cm"),
+                                row_names_gp = gpar(fontsize = 1),
+                                top_annotation = HeatmapAnnotation(Samples = col_lab,
+                                                                   Groups = col_group,
+                                                                   show_annotation_name = FALSE))
+  return(ht)
+}
+
+#### TAB 3 ####
 bar_plot <- function(gene_dropdown, df){
   ## TODO: Sort out Warning: Removed x rows containing missing values
   if (!(gene_dropdown %in% df$gene.names)) {
@@ -68,70 +134,4 @@ box_plot <- function(gene_dropdown, df){
           legend.text = element_text(size = 10, family = font_family),
           legend.position = "right ")
   return(p)
-}
-
-#### TAB 2 ####
-pca_plot <- function(df){
-  # create the group design
-  design <- data.frame("Samples" = seq(1:nrow(df)),
-                       "Group" = seq(1:nrow(df)))
-  design$Samples <- rownames(df)
-  design$Group <- gsub("_\\d+", "", design$Samples)
-
-  # Perform a PCA with pre-processed data
-  pca <- pca(X = df,
-             ncomp = nrow(df),
-             center = TRUE,
-             scale = TRUE)
-  # Create a 2D Sample Plot
-  p <- plotIndiv(object = pca,
-            comp = c(1,2),
-            ind.names = FALSE,
-            group = design$Group,
-            title = "Template data set",
-            legend = TRUE,
-            size.xlabel = rel(1.3),
-            size.ylabel = rel(1.3),
-            size.legend = rel(1.3),
-            cex = 0.8,
-            ellipse = TRUE)
-  return(p$graph)
-}
-
-#### TAB 3 ####
-function.heatmap <- function(matrix){
-  x <- as.matrix(matrix)
-  x.trunc <- as.vector(unique(x))
-  x.trim <- x.trunc[x.trunc >= quantile(x.trunc, probs = 0.01, na.rm = T) &
-                      x.trunc <= quantile(x.trunc, probs = 0.99, na.rm = T)]
-  x.max <- which.max(x.trim)
-  x.min <- which.min(x.trim)
-  # create a color function for the heatmap
-  col_fun <- circlize::colorRamp2(c(x.trim[x.min], 0, x.trim[x.max]),
-                                  c("blue", "white", "red"),
-                                  space = "sRGB")
-  # create labels for the heatmap
-  rowlab <- gsub(".*,\\s*", "", rownames(x)) # UniProt ID for row labels
-  col_lab <- gsub("_", " ", colnames(x)) # for sample annotation
-  col_group <- gsub("_\\d+", "", colnames(x)) |> # for group annotation
-    unlist() |> 
-    as.factor()
-  # create heatmap
-  set.seed(3)
-  ht <- ComplexHeatmap::Heatmap(x,
-                                name = "Protein level",
-                                col = col_fun,
-                                show_row_names = T,
-                                row_labels = rowlab,
-                                show_column_names = F,
-                                row_title = "Proteins",
-                                row_title_gp = grid::gpar(fontsize = 12,
-                                                          fontface = "bold"),
-                                column_title = "",
-                                row_dend_width = unit(2, "cm"),
-                                row_names_gp = gpar(fontsize = 1),
-                                top_annotation = HeatmapAnnotation(Samples = col_lab,
-                                                                   Groups = col_group,
-                                                                   show_annotation_name = FALSE))
-  return(ht)
 }
