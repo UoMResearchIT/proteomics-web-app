f43c5a357c020e39b943353f848aac8c7d795b13
fherreazcue <francisco.herreazcue@gmail.com>
2024-03-15 09:27:22 +0000

    Separates minio s3 data storage from shiny app data storage

--- R/on_message.R
+++ R/on_message.R
@@ -10,11 +10,11 @@ source("R/data_preprocessing.R")
 
 # Function to process messages
 process_message <- function(message) {
-  print("Received message:")
   #### Parse JSON ####
   data <- tryCatch({
     fromJSON(message)
   }, error = function(e) {
+    print("Received message:")
     print(message)
     print("ERROR: Cannot parse JSON:")
     print(e)
@@ -27,6 +27,7 @@ process_message <- function(message) {
 
   # Verify the keys date, time, topic and payload are present
   if (!all(c("date", "time", "topic", "payload") %in% names(data))) {
+    print("Received message:")
     print(message)
     print("ERROR: Missing keys in message.")
     print("  Required keys: date, time, topic, payload.")
@@ -35,12 +36,19 @@ process_message <- function(message) {
 
   # Verify the keys EventName and Key are present in the payload
   if (!all(c("EventName", "Key") %in% names(data$payload))) {
+    print("Received message:")
     print(message)
     print("ERROR: Missing keys in payload. Required keys: EventName, Key")
     return()
   }
 
+  # If accessed, ignore message
+  if (grepl("s3:ObjectAccessed", data$payload$EventName)) {
+    return()
+  }
+
   # Print recieved message
+  print("Received message:")
   print(paste(
     data$date,
     data$time,
@@ -57,22 +65,47 @@ process_message <- function(message) {
   ))
 
   #### Process message ####
+  # Creation event for any bucket
+  if (data$payload$EventName == "s3:ObjectCreated:Put") {
+    on_creation(data$payload$Key)
+  }
+  # Deletion event for any bucket
+  if (data$payload$EventName == "s3:ObjectRemoved:Delete") {
+    on_deletion(data$payload$Key)
+  }
+
   # Creation/Deletion events on raw-data dir
   if (data$topic == "raw-data") {
     if (data$payload$EventName == "s3:ObjectCreated:Put") {
-      on_creation(data$payload$Key)
+      on_raw_data_added(data$payload$Key)
     } else if (grepl("s3:ObjectRemoved:Delete", data$payload$EventName)) {
-      on_deletion(data$payload$Key)
+      on_raw_data_deleted(data$payload$Key)
     } else {
       print(paste("Nothing to do with detected event: ",
                   data$payload$EventName))
     }
-  } else {
-    print(paste("Nothing to do with detected topic: ", data$topic))
   }
+  # else {
+  #   print(paste("Nothing to do with detected topic: ", data$topic))
+  # }
 }
 
 on_creation <- function(file) {
+  system(paste("mc cp ",
+               "protein/", file,
+               " data/", file,
+               sep = ""))
+  print(paste("File: ", file, "added to shiny app data storage."))
+}
+
+on_deletion <- function(file) {
+  system(paste("rm ",
+               "data/", file,
+               sep = ""))
+  print(paste("File: ", file, "deleted from shiny app data storage."))
+}
+
+on_raw_data_added <- function(file) {
   file_name <- gsub("raw-data/", "", file)
   print(paste("Added raw data file: ", file_name))
   # Pre-process data
@@ -81,14 +114,13 @@ on_creation <- function(file) {
   # Push each file to the minio bucket
   for (file in files) {
     system(paste("mc cp ",
-                 "/tmp/", file,
+                 "./data/", file,
                  " protein/", dirname(file),
                  sep = ""))
   }
   print("Raw data pre-processed and pushed successfully.")
 }
-
-on_deletion <- function(file) {
+on_raw_data_deleted <- function(file) {
   file_name <- gsub("raw-data/", "", file)
   print(paste("Deleted raw data file: ", file_name))
   # Delete related pre-processed data files
--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,8 +1,16 @@
 # This script should bootstrap the backend for data storage and pre-processing
 # It generates a random password for the mqtt broker, launches the docker compose
-# network, and finalises configuration of the broker and minio buckets.
+# network, and finalises configuration of the broker and minio client and buckets.
 #
 # It should only be run once! If it fails, cherry pick the commands you need.
+#
+# IMPORTANT!!
+# Before running this script generate the access keys for the minio client:
+#   Run: docker compose up minio -d
+#   Go to http://localhost:9001/access-keys and use the 'create' button.
+#   Hit 'Create' again, and then 'Download for import'.
+#   Save the file as .secret_credentials.json in config/minio/
+#   Then run this script.
 
 #!/bin/bash
 
@@ -10,6 +18,9 @@
 set -e
 # Change the current directory to the directory of the script
 cd "$(dirname "$0")"
+# Fix secret credentials url. Should be "http://localhost:9000"
+sed -i 's#9001/api/v1/service-account-credentials#9000#g' minio/.secret_credentials.json
+
 # Generate random password
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 > .secret_passwd
 # Launch docker compose newtork
@@ -30,6 +41,8 @@ fi
 echo "."
 sleep 2
 cd config
+# Add mc alias
+mc alias import protein ./minio/.secret_credentials.json
 # Configure rabbitmq username and password
 ./rabbitmq/user_setup.sh
 # Configure minio buckets
@@ -39,5 +52,7 @@ cd config
 # To undo the important steps in the script do:
 # Teardown minio buckets
 #   ./minio/one_time_setup.sh -T
+# Remove the minio alias
+#   mc alias remove protein
 # Teardown docker compose network and forget rabbit configuration
 #   docker compose down -v
