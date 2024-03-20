f0a53dfccda594f9cb905f6f6337e4858601e839
fherreazcue <francisco.herreazcue@gmail.com>
2024-03-20 12:02:03 +0000

    fixes error detection for on_message and prevents false success messages

--- db/R/on_message.R
+++ db/R/on_message.R
@@ -69,7 +69,7 @@ process_message <- function(message) {
   #### Process message ####
   # Creation event for any bucket
   if (data$payload$EventName == "s3:ObjectCreated:Put") {
-    on_creation(data$payload$Key)
+    onc_es <- on_creation(data$payload$Key)
   }
   # Deletion event for any bucket
   if (grepl("s3:ObjectRemoved:Delete", data$payload$EventName)) {
@@ -79,7 +79,9 @@ process_message <- function(message) {
   # Creation/Deletion events on raw-data dir
   if (data$topic == "minio/raw-data") {
     if (data$payload$EventName == "s3:ObjectCreated:Put") {
-      on_raw_data_added(data$payload$Key)
+      if (onc_es == 0) {
+        on_raw_data_added(data$payload$Key)
+      }
     } else if (grepl("s3:ObjectRemoved:Delete", data$payload$EventName)) {
       on_raw_data_deleted(data$payload$Key)
     } else {
@@ -93,18 +95,32 @@ process_message <- function(message) {
 }
 
 on_creation <- function(file) {
-  system(paste("mc cp ",
-               "protein/", file, " ",
-               data_path, file,
-               sep = ""))
-  print(paste("File: ", file, "added to shiny app data storage."))
+  mc_command <- paste("mc cp ",
+                      "protein/", file, " ",
+                      data_path, file,
+                      sep = "")
+  exit_status <- system(mc_command)
+
+  if (exit_status == 0) {
+    print(paste("File: ", file, "added to shiny app data storage."))
+  } else {
+    print(paste("Error copying file: ", file))
+  }
+  return(exit_status)
 }
 
 on_deletion <- function(file) {
-  system(paste("rm ",
-               data_path, file,
-               sep = ""))
-  print(paste("File: ", file, "deleted from shiny app data storage."))
+  command <- paste("rm ",
+                   data_path, file,
+                   sep = "")
+  exit_status <- system(command)
+
+  if (exit_status == 0) {
+    print(paste("File: ", file, "deleted from shiny app data storage."))
+  } else {
+    print(paste("Error deleting file: ", file))
+  }
+  return(exit_status)
 }
 
 on_raw_data_added <- function(file) {
@@ -114,22 +130,33 @@ on_raw_data_added <- function(file) {
   print("Pre-processing data...")
   files <- preprocess_data(raw_data_path = paste(data_path, file, sep = ""))
   # Push each file to the minio bucket
+  errors <- 0
   for (file in files) {
-    print(paste("mc cp ",
-                 file, " ",
-                 "protein/", dirname(file),
-                 sep = ""))
-    system(paste("mc cp ",
-                 file, " ",
-                 "protein/", gsub("../app/data/", "", dirname(file)),
-                 sep = ""))
+    mc_command <- paste("mc cp ",
+                        file, " ",
+                        "protein/", gsub("../app/data/", "", dirname(file)),
+                        sep = "")
+    exit_status <- system(mc_command)
+
+    if (exit_status == 0) {
+      print(paste("File: ", file, "pushed successfully."))
+    } else {
+      print(paste("Error pushing file: ", file))
+      errors <- errors + 1
+    }
   }
-  print("Raw data pre-processed and pushed successfully.")
+
+  if (errors > 0) {
+    print(paste("Errors pushing ", errors, " files."))
+  } else {
+    print("Raw data pre-processed and pushed successfully.")
+  }
+  return(errors)
 }
 on_raw_data_deleted <- function(file) {
   file_name <- gsub("raw-data/", "", file)
   dataset_name <- gsub(".txt", "", file_name)
-  print(paste("Deleted raw data file: ", file_name))
+  print(paste("Detected delition of raw data file: ", file_name))
   # Delete related pre-processed data files
   print("Deleting related files...")
   related_files <- list(
@@ -138,10 +165,25 @@ on_raw_data_deleted <- function(file) {
     c("pcaplots", "_pca.xlsx"),
     c("dataset-info", "_info.md")
   )
+  errors <- 0
   for (rel in related_files){
-    system(paste("mc rm protein/", rel[1], "/",
-                 dataset_name, rel[2],
-                 sep = ""))
+    command <- paste("mc rm protein/", rel[1], "/",
+                     dataset_name, rel[2],
+                     sep = "")
+    exit_status <- system(command)
+
+    if (exit_status == 0) {
+      print(paste("File: ", file, "removed successfully."))
+    } else {
+      print(paste("Error removing file: ", file))
+      errors <- errors + 1
+    }
   }
-  print("Raw data and related files deleted.")
+
+  if (errors > 0) {
+    print(paste("Errors removing ", errors, " files."))
+  } else {
+    print("Raw data and related files deleted.")
+  }
+  return(errors)
 }
